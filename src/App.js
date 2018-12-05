1966164a722cc64b6f76a0916755f07315402ca8
Shahadul Haider <shahadul.haider@gmail.com>
2018-12-05 19:59:52 +0600

    header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from './components/Header'
+import './assets/styles.css';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Welcome to the Venue</h1>
+      <div className="App" style={{ height: "1580px", background: "cornflowerblue" }}>
+        <Header />
       </div>
     );
   }
