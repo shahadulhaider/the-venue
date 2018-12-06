863c5f2230117909cac2022f280d8bb5679f339e
Shahadul Haider <shahadul.haider@gmail.com>
2018-12-06 12:22:29 +0600

    carrousel and countdown added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import Header from './components/Header'
+import Header from './components/Header';
+import Featured from './components/featured';
 import './assets/styles.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App" style={{ height: "1580px", background: "cornflowerblue" }}>
         <Header />
+        <Featured />
       </div>
     );
   }
